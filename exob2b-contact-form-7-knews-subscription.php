b35e85dfddcf155ed39fe2f8b43b708cdb9371d7
Mathieu Forest <forestmathieu@gmail.com>
2014-11-28 10:34:43 -0500

    Update exob2b-contact-form-7-knews-subscription.php

--- exob2b-contact-form-7-knews-subscription.php
+++ exob2b-contact-form-7-knews-subscription.php
@@ -9,21 +9,24 @@
 Contact form 7 form to knews subscriptions. You need to install Dynamic hidden text field plugin. Add this shortcodes with the list id: [dynamictext knews-subscription "Put the list id here"]. And add those field [email] [name] and [surname].
  */
 
-function wpcf7_knews_subscription (&$WPCF7_ContactForm) {
-
-	if ($WPCF7_ContactForm->posted_data['knews-subscription'] == "1") {
+function wpcf7_knews_subscription ($WPCF7_ContactForm) {
 	
-		$email = $WPCF7_ContactForm->posted_data['email'];
+	$submission = WPCF7_Submission::get_instance();
+	$posted_data =& $submission->get_posted_data();	   
+
+	if(isset($posted_data['knews-subscription'])) {
+
+		$email = $posted_data['email'];
 		
-		$id_list_news = $WPCF7_ContactForm->posted_data['knews-subscription'];
+		$id_list_news = $posted_data['knews-subscription'];
 		$lang = ICL_LANGUAGE_CODE;
 		$lang_locale = get_locale();
 		
-		$extra_fields = $WPCF7_ContactForm->posted_data;
+		$extra_fields = $posted_data;
 		$custom_fields = array();
 			foreach ($extra_fields as $field) {
-				$custom_fields[1]=$WPCF7_ContactForm->posted_data['name'];
-				$custom_fields[2]=$WPCF7_ContactForm->posted_data['surname'];
+				$custom_fields[1]=$posted_data['name'];
+				$custom_fields[2]=$posted_data['surname'];
 			}
 	
 		$bypass_confirmation = true;
@@ -32,6 +35,7 @@ function wpcf7_knews_subscription (&$WPCF7_ContactForm) {
 			
 	}	
 }
-add_action("wpcf7_before_send_mail", "wpcf7_knews_subscription");
+
+add_action('wpcf7_before_send_mail', 'wpcf7_knews_subscription');
 
 ?>
